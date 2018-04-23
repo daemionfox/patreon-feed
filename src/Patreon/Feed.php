e62bf4af1ca6da7d569e8da8a3823312ddb1f5d5
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2018-04-23 19:22:38 +0200

    Feed constructor for private feed

--- src/Patreon/Feed.php
+++ src/Patreon/Feed.php
@@ -94,12 +94,23 @@ class Feed extends PatreonRSS
     /** @noinspection PhpMissingParentConstructorInspection */
     /**
      * Feed constructor.
+     * If password is given, treat creator name as login email.
      * @param null $creator
+     * @param null $password
      */
-    public function __construct($creator = null)
+    public function __construct($creator = null, $password = null)
     {
-        if ($creator !== null) {
-            $this->setCreator($creator);
+        if ($creator !== null)
+        {
+            if($password === null)
+            {
+                $this->setCreator($creator);
+            }
+            else
+            {
+                $this->login($creator, $password);
+                $this->getPatreon();
+            }
         }
     }
 
